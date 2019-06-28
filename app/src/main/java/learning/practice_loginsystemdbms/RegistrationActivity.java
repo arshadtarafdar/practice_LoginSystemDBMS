a09a22b21e09a981e13cd5d06c30cfbb7583dcff
Arshad <arshad.tarafdar14@gmail.com>
2019-06-28 15:02:01 +0530

    Initial commit

---
+++ app/src/main/java/learning/practice_loginsystemdbms/RegistrationActivity.java
@@ -0,0 +1,92 @@
+package learning.practice_loginsystemdbms;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText userName, userEmail, userPassword;
+    private Button regButton;
+    private TextView userLogin;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        setupUIViews();
+        firebaseAuth = FirebaseAuth.getInstance();
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate();
+                if(validate())
+                {
+                    String user_email = userEmail.getText().toString().trim();
+                    String user_password = userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(RegistrationActivity.this, "Registered Successfully!", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                            }
+                            else {
+                                Toast.makeText(RegistrationActivity.this, "User not Registered!", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void setupUIViews()
+    {
+        userName = (EditText) findViewById(R.id.etUserName);
+        userEmail = (EditText) findViewById(R.id.etUserEmail);
+        userPassword = (EditText) findViewById(R.id.etUserPassword);
+        regButton = (Button) findViewById(R.id.btnRegister);
+        userLogin = (TextView) findViewById(R.id.UserLogin);
+    }
+
+    private boolean validate()
+    {
+        Boolean result = true;
+
+        String name = userName.getText().toString();
+        String password = userPassword.getText().toString();
+        String email = userEmail.getText().toString();
+
+        if(name.isEmpty() || password.isEmpty() || email.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(), "Please check the fields before registering.", Toast.LENGTH_SHORT).show();
+            result = false;
+            //return result;
+        }
+
+        return result;
+    }
+}
---
+++ app/src/main/java/learning/practice_loginsystemdbms/SecondActivity.java
@@ -0,0 +1,13 @@
+package learning.practice_loginsystemdbms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
